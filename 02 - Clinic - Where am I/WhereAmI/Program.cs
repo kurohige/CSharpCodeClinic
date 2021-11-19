32eceea3d0c6c3e212bc191a41c9cb876e6276dd
Jose Hidalgo <joh2j@mtmail.mtsu.edu>
2021-11-19 19:04:16 +0200

    Where Am I project for the C# Code Clinic course on Linkedin, information regarding API was not added to the project on purpose.

---
+++ 02 - Clinic - Where am I/WhereAmI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Device.Location;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting GeoCoordinate Watcher...");
+
+            // 1. Add a Reference to System.Device.dll
+
+            // 2. Use the GeoCoordinate Watcher
+            var watcher = new GeoCoordinateWatcher();
+
+            watcher.StatusChanged += (s, e) =>
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:StatusChanged:{e.Status}");
+            };
+
+            watcher.PositionChanged += (s, e) =>
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:PositionChangeD:{e.Position.Location}");
+            };
+
+            watcher.MovementThreshold = 100;
+
+            watcher.Start();
+
+            // 3. Use the Map Image REST API
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
